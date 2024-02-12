859267007be75f65ef396150343b003cfc5d29d6
Hidenny <2021-0208@uteco.edu.do>
2024-02-12 07:14:13 -0400

    New migration for the invoice

---
+++ Migrations/20240212003245_NewPropertyInvoice.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FactuSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPropertyInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TypePayment",
+                table: "Facturas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypePayment",
+                table: "Facturas");
+        }
+    }
+}
